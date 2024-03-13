79ee8f4da27f535cfded8ddb5a20a4aea0857968
fr4nk <porameht@outlook.com>
2024-03-14 00:40:23 +0700

    add cron 1h

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello from FastAPI!"}
+  
+@app.get("/api/cron/1h")
+async def cron():
+    return {"message": "Hello cron 1h from FastAPI!"}
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
